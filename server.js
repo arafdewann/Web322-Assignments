fe7a11d124e2120f84e7e49cfd383c9193b97638
MD ARAFAT KOYES <125794297+arafdewann@users.noreply.github.com>
2024-09-23 20:53:22 -0400

    Update server.js

--- server.js
+++ server.js
@@ -2,20 +2,21 @@ const express = require('express'); // "require" the Express module
 const app = express(); // obtain the "app" object
 const HTTP_PORT = process.env.PORT || 3000; // assign a port
 
-// get index Route
+// get index Route - Display your full name and student number
 app.get('/', (req, res) => {
-  res.send("Hello world");
+  res.send("MD ARAFAT KOYES - 123456789");
 });
 
 // get contact Route
 app.get('/contact', (req, res) => {
-  res.send("Hello world");
+  res.send("Contact Page");
 });
 
 // get about Route
 app.get('/about', (req, res) => {
-  res.send("Hello world");
+  res.send("About Page");
 });
 
 // start the server on the port and output a confirmation to the console
 app.listen(HTTP_PORT, () => console.log(`server listening on: ${HTTP_PORT}`));
+
