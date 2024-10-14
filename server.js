a1f9c78eb25de20c79b65f3c9016df8af2e32bc3
MD ARAFAT KOYES <125794297+arafdewann@users.noreply.github.com>
2024-10-14 03:13:10 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 const express = require('express'); // "require" the Express module
 const app = express(); // obtain the "app" object
 const HTTP_PORT = process.env.PORT || 3000; // assign a port
@@ -32,4 +32,4 @@ app.get('/', (req, res) => {
 app.listen(HTTP_PORT, () => {
   console.log(`Server is running on port ${HTTP_PORT}`);
 });
->>>>>>> 88797b9ad9bcd0ed49bff9f511612944e7206902
+
